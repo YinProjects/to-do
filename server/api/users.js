bc18ade2f58b8b29cb0e15812bf4d25ae24bc07b
Olivia Liao <ol64@cornell.edu>
2022-07-27 11:27:30 -0400

    login form

--- server/api/users.js
+++ server/api/users.js
@@ -1,29 +1,37 @@
-const router = require('express').Router()
-const { models: { User }} = require('../db')
-module.exports = router
+const router = require('express').Router();
+const {
+	models: { User },
+} = require('../db');
+module.exports = router;
 
 router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.findAll({
-      attributes: ['id', 'username']
-    })
-    res.json(users)
-  } catch (err) {
-    next(err)
-  }
-})
+	try {
+		const users = await User.findAll({
+			attributes: ['id', 'username'],
+		});
+		res.json(users);
+	} catch (err) {
+		next(err);
+	}
+});
 
+router.post('/user', async (req, res, next) => {
+	try {
+		const user = await User.findOne({
+			where: {
+				username: req.body.username,
+			},
+			attributes: ['id', 'username'],
+		});
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const users = await User.findOne({
-      where: {
-        id: req.params.id
-      },
-      attributes: ['id', 'username']
-    })
-    res.json(users)
-  } catch (err) {
-    next(err)
-  }
-})
+		if (!user || user.password !== req.body.password) {
+			const error = Error('Incorrect username/password');
+			error.status = 401;
+			throw error;
+		}
+
+		res.json(user);
+	} catch (err) {
+		next(err);
+	}
+});
